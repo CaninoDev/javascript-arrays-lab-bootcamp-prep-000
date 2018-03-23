ee92d5fa85165b1df919f0f9ff2f39f2a475694e
Ian Canino <ian.canino@gmail.com>
2018-03-23 02:27:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,3 +27,11 @@ function appendKitten(name) {
 function prependKitten(name) {
   return ([name, ...kittenS])
 }
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1, kittens.length)
+}
